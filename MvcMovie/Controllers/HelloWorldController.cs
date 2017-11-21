214f6086ddaf74717b6fcd3653b52cade0291d43
Hai Van Tran <haitran@2cs.local>
2017-11-21 14:45:28 +0700

    Modify the code to pass some parameter information from the URL to the controller

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -15,10 +15,10 @@ namespace MvcMovie.Controllers
 
         // 
         // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome()
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
